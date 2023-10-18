7786b9d6d69b46ca5d4a2d0c86d6ed3884831008
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2023-10-18 21:21:28 +0000

    PHP setup: improve security
    Configure PHP-FPM to only execute exact matches for files rather than closest
    
    Make shelcheck happy

--- php-setup.sh
+++ php-setup.sh
@@ -3,10 +3,18 @@
 #set -eux
 shopt -s dotglob
 
-cd /vagrant
+# shellcheck source=.env
+cd /vagrant || exit
+
+# shellcheck source=.env
 source .env
 
 cp -vf /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.ini.dist
-cp -vf /vagrant/php.ini /etc/php/7.4/fpm/php.ini
+cp -vf php.ini /etc/php/7.4/fpm/php.ini
+# Configure PHP-FPM to only execute exact matches for files rather than closest
+SEARCH=";cgi.fix_pathinfo=.*"
+REPLACE="cgi.fix_pathinfo=1"
+FILEPATH="/etc/php/7.0/fpm/php.ini"
+sudo sed -i "s|$SEARCH|$REPLACE|" $FILEPATH
 
 nginx -s reload

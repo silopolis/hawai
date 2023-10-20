3f8e970e54fd197198ff61a433f37b457c7987c6
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2023-10-20 15:33:32 +0000

    Add Python virtual env and requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,31 @@
 #set -eux
 shopt -s dotglob
 
+echo "-- Source '.env' configuration file"
 source .env
 
+if [ ! $(dpkg -l python3-venv | grep -q "^ii") ]; then
+  echo "-- Install Python venv"
+  sudo apt install python3-venv;
+fi
+if [ ! -d .venv ]; then
+  echo "-- Create Python '.venv' virtual environment"
+  python3 -m venv .venv
+fi
+echo "-- Activate Python '.venv' virtual environment"
+source .venv/bin/activate
+echo "-- Upgrage Python pip and setuptools"
+pip install --upgrade setuptools pip
+echo "-- Install Python requirements"
+pip install -r requirements.txt
+
+echo "-- Build docker images"
 for i in base mariadb nginx lep lemp; do
-  echo "-- Building '$i' docker image"
+  echo "-- Build '$i' docker image"
   docker buildx build -t silopolis:$i -f $i.dockerfile .;
 done
 
-echo "-- Boostraping Vagrant environment"
+echo "-- Boostrap Vagrant environment"
 vagrant up --no-parallel
 # vagrant up networks
 # vagrant up admin1
--- destroy.sh
+++ destroy.sh
@@ -5,9 +5,19 @@ shopt -s dotglob
 
 source .env
 
+echo "-- Destroy Vagrant environment"
 vagrant destroy -f
-sudo rm -vrf data/maria1/mysql/*
-sudo rm -vrf data/wpress1/wordpress/*
+rm -rf .vagrant/*
+
+echo "-- Prune docker environment"
 docker -D container prune -f
 docker -D image prune -f
 docker -D network prune -f
+
+echo "-- Delete data"
+sudo rm -rf data/maria1/mysql/*
+sudo rm -rf data/wpress1/wordpress/*
+
+# echo "-- Purge Python virtual environment"
+# deactivate
+# rm -rf .venv

30183e1ff13a864c586867dc7b282c710dbcdefa
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2023-10-23 19:23:55 +0000

    scripts: destroy: really cleanup docker environment

--- destroy.sh
+++ destroy.sh
@@ -9,10 +9,17 @@ echo "-- Destroy Vagrant environment"
 vagrant destroy -f
 rm -rf .vagrant/*
 
-echo "-- Prune docker environment"
-docker -D container prune -f
-docker -D image prune -f
-docker -D network prune -f
+echo "-- Remove Docker containers"
+#docker -D container prune -f
+docker rm "$(docker container ls -q)" -f
+echo "-- Remove Docker images"
+#docker -D image prune -f
+docker image rm "$(docker images -aq)" -f
+echo "-- Remove Docker networks"
+#docker -D network prune -f
+docker network rm "$(docker network ls |grep vagrant |cut -d' ' -f1)" -f
+echo "-- Purge buildx cache"
+docker buildx prune -af
 
 echo "-- Delete data"
 sudo rm -rf data/maria1/mysql/*

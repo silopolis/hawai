b678481149d64606e318670bff2ab9266648cec9
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2023-10-23 19:21:13 +0000

    scripts: bootstrap: refactor
    Split out Python setup, Vagrant setup and Docker image building
    into dedicated scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-#set -eux
-shopt -s dotglob
+set -e #ux
 
+./scripts/python_setup.sh
+
+echo "-- Source shell config in case it's been updated"
+source ~/.profile
+echo "-- Activate Python virtual environment"
+source .venv/bin/activate
 echo "-- Source '.env' configuration file"
 source .env
 
-if [ ! $(dpkg -l python3-venv | grep -q "^ii") ]; then
-  echo "-- Install Python venv"
-  sudo apt -qq -y install python3-venv;
-fi
-if [ ! -d .venv ]; then
-  echo "-- Create Python '.venv' virtual environment"
-  python3 -m venv .venv
-fi
-echo "-- Activate Python '.venv' virtual environment"
-source .venv/bin/activate
-echo "-- Upgrage Python pip and setuptools"
-pip install --upgrade setuptools pip
-echo "-- Install Python requirements"
-pip install -r requirements.txt
+./docker/build_images.sh
 
-echo "-- Build docker images"
-for i in base mariadb nginx lep lemp; do
-  echo "-- Build '$i' docker image"
-  docker buildx build -t silopolis:$i -f docker/$i.dockerfile .;
-done
-
-echo "-- Install Vagrant plugins"
-vagrant plugin install vagrant-scp #vagrant-env vagrant-hostmanager
+./scripts/vagrant_setup.sh
 
 echo "-- Boostrap Vagrant environment"
 vagrant up --no-parallel

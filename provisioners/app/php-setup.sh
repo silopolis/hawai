e548363112e257e24901ede58c41e37fb263fcf1
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2023-10-24 10:28:24 +0000

    prov: php: sync with last commited conf

--- provisioners/app/php-setup.sh
+++ provisioners/app/php-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#set -eux
-shopt -s dotglob
+set -e #ux
 
 # shellcheck source=.env
 cd /vagrant || exit
@@ -9,12 +8,13 @@ cd /vagrant || exit
 # shellcheck source=.env
 source .env
 
-cp -vf /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.ini.dist
-cp -vf php.ini /etc/php/7.4/fpm/php.ini
+cp -vf "/etc/php/$APP_PHP_VERSION/fpm/php.ini" \
+  "/etc/php/$APP_PHP_VERSION/fpm/php.ini.dist"
+cp -vf conf/app/php.ini "/etc/php/$APP_PHP_VERSION/fpm/php.ini"
 # Configure PHP-FPM to only execute exact matches for files rather than closest
-SEARCH=";cgi.fix_pathinfo=.*"
-REPLACE="cgi.fix_pathinfo=1"
-FILEPATH="/etc/php/7.0/fpm/php.ini"
-sudo sed -i "s|$SEARCH|$REPLACE|" $FILEPATH
+# SEARCH=".*cgi.fix_pathinfo=.*"
+# REPLACE="cgi.fix_pathinfo=1"
+# FILEPATH="/etc/php/$APP_PHP_VERSION/fpm/php.ini"
+# sudo sed -i "s|$SEARCH|$REPLACE|" "$FILEPATH"
 
 nginx -s reload
